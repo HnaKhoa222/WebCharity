80ef34ea545e3dd8cf6c7bb8e5db578b7d87fccf
Son <sondangbao412@gmail.com>
2025-03-26 10:20:29 +0700

    add Guidepage

--- webcharity/src/App.js
+++ webcharity/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { auth } from './firebase';
-import AuthPage from './components/AuthPage/AuthPage';
+import { auth } from "./firebase";
+import AuthPage from "./components/AuthPage/AuthPage";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Create from "./pages/Create";
-import Guide from "./pages/Guide";
+import Guide from "./pages/Guide/Guide";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
